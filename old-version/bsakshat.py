0cb4c31a17b3030fac7fa4f5180aef1ce6aa8f3c
Sakshat Bhattarai <bhattaraisakshat07@gmail.com>
2020-06-13 01:12:37 -0400

    old-version branch

---
+++ old-version/bsakshat.py
@@ -0,0 +1,13 @@
+# A simple program for the webpage
+# Sakshat Bhattarai python program
+
+def main():
+    print("Hello World!")
+    name = input("Enter your name:") #Input the user's name
+    print("Hi,", name)
+    print("This is Sakshat.")
+    print("Welcome to my site!")
+
+
+if __name__ == "__main__":
+    main()
