082740640f49a629bc40c0c859dfeaa76ef9f2e5
Sakshat Bhattarai <bhattaraisakshat07@gmail.com>
2020-06-16 09:03:52 -0400

    react spa

---
+++ precache-manifest.8f102e0c15a2ffeed9b3594653a09702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06595a5fa31c52a9b9335a90ff9c50bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91d70c9233ded241e425",
+    "url": "/static/css/main.c252e569.chunk.css"
+  },
+  {
+    "revision": "64cd6b631345fbacdb24",
+    "url": "/static/js/2.9a408c6f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9a408c6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91d70c9233ded241e425",
+    "url": "/static/js/main.b402bc5d.chunk.js"
+  },
+  {
+    "revision": "1236787694ae95063e83",
+    "url": "/static/js/runtime-main.ff1f327d.js"
+  }
+]);
